4369498ff88648307f14a0da667da7c2f11e3efb
Oliver Szabo <oleewere@gmail.com>
2017-09-07 16:29:44 +0200

    init application

---
+++ src/main/java/org/apache/ambari/solr/metrics/task/SceduledSolrMetricsTask.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ambari.solr.metrics.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SceduledSolrMetricsTask {
+
+  private static final Logger log = LoggerFactory.getLogger(SceduledSolrMetricsTask.class);
+
+  @Scheduled(fixedDelayString = "${infra-solr.metrics.push.rate}")
+  public void reportCurrentTime() {
+    log.info("Do something...");
+  }
+
+}
